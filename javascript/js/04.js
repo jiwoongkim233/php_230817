636bf0d748bd5bda7ee8951c5764bf4874d0bc9a
jiwoongkim233 <rlawldnd841@gmail.com>
2023-10-23 18:04:04 +0900

    20231023_javascript

---
+++ javascript/js/04.js
@@ -0,0 +1,20 @@
+// console.log("반갑습니다. JS파일입니다.");
+
+// 변수(var, let, const) 
+// var : 중복선언 가능, 재할당 가능, 함수레벨 스코프
+
+// var u_name = "홍길동";
+// console.log(u_name);
+// u_name = "갑순이";
+// console.log(u_name);
+
+// let : 중복 선언 불가능, 재할당 가능, 블록 레벨 스코프
+let u_name = "홍길동";
+console.log(u_name);
+u_name = "갑순이";
+console.log(u_name);
+
+// const : 중복 선언 불가능, 재할당 불가능, 블록 레벨 스코프
+const AGE = 19;
+
+\
